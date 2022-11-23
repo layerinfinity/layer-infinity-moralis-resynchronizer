7c3db38d437c3a369c0a7bc8ace8a43c4a30c478
Quan Pham <doraemonfanclub@gmail.com>
2022-11-23 11:03:33 +0700

    Update addresses

--- src/sync.py
+++ src/sync.py
@@ -34,42 +34,42 @@ queue: list[ChainNFTSet] = []
 queue.append(
     ChainNFTSet(
         chain="goerli",
-        tokenAddress="0xC99e68b19B8eAdd849317A8221ecF55185B0a400",
+        tokenAddress="0x41CE00De36E94fbFE06D694A7e14d7C514256D56",
         tokenId=1,
     )
 )
 queue.append(
     ChainNFTSet(
         chain="avalanche testnet",
-        tokenAddress="0x87316fBDCe129b287420431d36f1182412B4b70d",
+        tokenAddress="0x9d9d99f32294215fbd40279367D50d74BC185825",
         tokenId=1,
     )
 )
 queue.append(
     ChainNFTSet(
         chain="mumbai",
-        tokenAddress="0x4a132EcaBBdF61E34f14b513D0c4845468c6BA18",
+        tokenAddress="0x2E478697c9E403938C9076e00d0b15680536F4A4",
         tokenId=1,
     )
 )
 queue.append(
     ChainNFTSet(
         chain="eth",
-        tokenAddress="0xEfe075c5cA6342a09cC9715D823C56465E6a6dbC",
+        tokenAddress="0x90B9a29628Dc1b2674777f6293490ebE3289F2EF",
         tokenId=1,
     )
 )
 queue.append(
     ChainNFTSet(
         chain="avalanche",
-        tokenAddress="0x358A609653D05153901F8791347f357E3ec58cC5",
+        tokenAddress="0x90B9a29628Dc1b2674777f6293490ebE3289F2EF",
         tokenId=1,
     )
 )
 queue.append(
     ChainNFTSet(
         chain="polygon",
-        tokenAddress="0x358A609653D05153901F8791347f357E3ec58cC5",
+        tokenAddress="0x90B9a29628Dc1b2674777f6293490ebE3289F2EF",
         tokenId=1,
     )
 )
@@ -115,6 +115,51 @@ queue.append(
     ChainNFTSet(
         chain="polygon",
         tokenAddress="0xD4A554B125672CE8dA053205bC9DA4E26aA78f44",
+        tokenId=1,
+    )
+)
+
+# Fusion
+queue.append(
+    ChainNFTSet(
+        chain="goerli",
+        tokenAddress="0xd9A50766CCF5cd17ef643dfc89a4129ab90D2537",
+        tokenId=1,
+    )
+)
+queue.append(
+    ChainNFTSet(
+        chain="avalanche testnet",
+        tokenAddress="0x0F9bCea9317735437bF7b1f1ccA28281D01cF686",
+        tokenId=1,
+    )
+)
+queue.append(
+    ChainNFTSet(
+        chain="mumbai",
+        tokenAddress="0x71881245F6a512BefD5DD0d89688171F56Fb8f59",
+        tokenId=1,
+    )
+)
+
+queue.append(
+    ChainNFTSet(
+        chain="eth",
+        tokenAddress="0x5c79FC6ed88a3BB36c0E0abefF56dCE6D7E74176",
+        tokenId=1,
+    )
+)
+queue.append(
+    ChainNFTSet(
+        chain="avalanche",
+        tokenAddress="0x5c79FC6ed88a3BB36c0E0abefF56dCE6D7E74176",
+        tokenId=1,
+    )
+)
+queue.append(
+    ChainNFTSet(
+        chain="polygon",
+        tokenAddress="0x5c79FC6ed88a3BB36c0E0abefF56dCE6D7E74176",
         tokenId=1,
     )
 )
